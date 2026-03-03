93afd5ac56243be79cf695138a75cf13332598e4
ruanlatorre <ruan.latorre@aluno.senai.br>
2026-03-03 10:00:29 -0300

    Implantar padronizacao de classe

--- index.php
+++ index.php
@@ -6,7 +6,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Login - SENAI MANUTENÇÃO</title>
-    
+
     <!-- Estilização, BootstrapIcons e Favicon -->
     <link rel="stylesheet" href="style.css">
     <link rel="stylesheet" href="css/modal.css">
@@ -52,12 +52,14 @@
                     <div class="div-input">
                         <i class="bi bi-envelope-fill"></i>
                         <input type="email" id="email" name="email" placeholder="E-mail" class="input">
-                        <button type="button" style="visibility: hidden;" id="btnScan1" class="btnEsp"><i class="bi bi-qr-code-scan"></i></button>
+                        <button type="button" style="visibility: hidden;" id="btnScan1" class="btnEsp"><i
+                                class="bi bi-qr-code-scan"></i></button>
                     </div>
                     <div class="div-input">
                         <i class="bi bi-shield-fill"></i>
                         <input type="password" id="senha" name="senha" placeholder="*****" class="input">
-                        <button type="button" onclick="showPass()" id="btnEyeLogin" class="btnEsp"><i class="bi bi-eye-fill"></i></button>
+                        <button type="button" onclick="showPass()" id="btnEyeLogin" class="btnEsp"><i
+                                class="bi bi-eye-fill"></i></button>
                     </div>
 
                     <div class="div-btn">

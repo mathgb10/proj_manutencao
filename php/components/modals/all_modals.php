11c7887a9a4311f4a3d95efc0975ce07f5398998
matheus-benevides <matheus.g.benevides@aluno.senai.br>
2026-03-04 11:25:24 -0300

    ajustes na tabela

--- php/components/modals/all_modals.php
+++ php/components/modals/all_modals.php
@@ -3,14 +3,24 @@
     <div class="modal-fundo" id="acesso" style="display: flex;">
         <div class="modal-box-acesso">
             <div class="modal-header-acesso">
-                <h3>Acesso Negado</h3>
-                <button onclick="closeModal('acesso')"><i class="bi bi-x-lg"></i></button>
+                <!-- <button onclick="closeModal('acesso')"><i class="bi bi-x-lg"></i></button> -->
             </div>
             <div class="modal-txt-acesso">
-                <p>O seu nível de permissão não permite que você acesse está página.</p>
+                <div>
+                    <i class="bi bi-exclamation-diamond"></i>
+                    <h3>Acesso Negado</h3>
+                </div>
+                <?php if (basename($_SERVER['PHP_SELF']) == "index.php") {
+                ?>
+                    <p>Realize o login antes de tentar acessar nosso sistema.</p>
+                <?php
+                } else {
+                ?>
+                    <p>O seu nível de permissão não permite que você acesse está página.</p>
+                <?php } ?>
             </div>
             <div class="modal-btn-acesso">
-                <button onclick="closeModal('acesso')" class="btn">OK</button>
+                <button onclick="closeModal('acesso')" class="btn confirmar">OK</button>
             </div>
         </div>
     </div>
